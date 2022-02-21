c63dadd59ff3653af88184d069ec25e2012216f8
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-02-20 23:02:43 -0800

    add save to text file option

--- File Folder Count.py
+++ File Folder Count.py
@@ -2,10 +2,14 @@ import os
 
 path = input("Enter path >> ")
 exclude_under = int(input("Exclude folders with less than this many files >> "))
+save_file = input("Save to text file? (Y/N) >> ")
+if save_file == "Y" or save_file == "y":
+    with open('File count.txt', 'w') as save:
+        save.write(path + '\n')
 total_files = 0
 total_dir = 0
 for base, dirs, files in os.walk(path):
-    dirs =  sorted(dirs)
+    dirs = sorted(dirs)
     file_count = 0
     for directories in dirs:
         total_dir += 1
@@ -13,6 +17,12 @@ for base, dirs, files in os.walk(path):
         total_files += 1
         file_count += 1
     if file_count >= exclude_under:
+        with open('File count.txt', 'a') as save:
+            save.write(base + ' : ' + str(file_count) + ' files \n')
         print(base, " : ", file_count, " files")
 print("Total files: ", total_files)
 print("Total directories: ", total_dir)
+with open('File count.txt', 'a') as save:
+    save.write("Total files: " + str(total_files) + '\n')
+    save.write("Total directories: " + str(total_dir) + '\n')
+save.close()
