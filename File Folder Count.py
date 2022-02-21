85b11b5d028e86d44fef0a0d7686187b69247abb
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-02-20 21:25:56 -0800

    Create File Folder Count.py

---
+++ File Folder Count.py
@@ -0,0 +1,15 @@
+import os
+
+path = input("Enter path >> ")
+total_files = 0
+total_dir = 0
+for base, dirs, files in os.walk(path):
+    file_count = 0
+    for directories in dirs:
+        total_dir += 1
+    for Files in files:
+        total_files += 1
+        file_count += 1
+    print(base, " : ", file_count, " files")
+print("Total files: ", total_files)
+print("Total directories: ", total_dir)
